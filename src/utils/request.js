5aec076f684e57df213cf48d13de87894c760219
00zsq <2314755084@qq.com>
2026-03-14 19:35:10 +0800

    fix: 将待处理请求从对象改为 Map，以优化请求管理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,32 +1,32 @@
 import axios from 'axios'
 
-const pendingRequests = {}
+const pendingRequests = new Map()
 
 const instance = axios.create({
   baseURL: '',
   timeout: 1000000,
-  headers: {}
+  headers: {},
 })
 
 instance.interceptors.request.use(
-  config => {
+  (config) => {
     addPendingRequest(config)
     return config
   },
-  error => {
+  (error) => {
     return Promise.reject(error)
-  }
+  },
 )
 
 instance.interceptors.response.use(
-  response => {
+  (response) => {
     removePendingRequest(response.config)
     return response
   },
-  error => {
+  (error) => {
     removePendingRequest(error.config || {})
     return Promise.reject(error)
-  }
+  },
 )
 
 // 生成请求key
@@ -38,19 +38,19 @@ const generateRequestKey = (config) => {
 // 添加请求到pending
 const addPendingRequest = (config) => {
   const requestKey = generateRequestKey(config)
-  if (pendingRequests[requestKey]) {
+  if (pendingRequests.has(requestKey)) {
     config.cancelToken = new axios.CancelToken((cancel) => {
       cancel('请勿重复请求')
     })
   } else {
-    pendingRequests[requestKey] = true
+    pendingRequests.set(requestKey, true)
   }
 }
 
 // 移除pending中的请求
 const removePendingRequest = (config) => {
   const requestKey = generateRequestKey(config)
-  delete pendingRequests[requestKey]
+  pendingRequests.delete(requestKey)
 }
 
 export default instance
